bf8f1f1098e3c384cd790e95f2b299ba2cf7cef4
alan_omam <omamalan@hotmail.it>
2024-04-03 17:03:01 +0200

    creato un array che contiene tutti i miei prodotti

---
+++ db.php
@@ -0,0 +1,40 @@
+<?php
+
+require './Models/Prodotto.php';
+require './Models/Categoria.php';
+require './Models/Cibo.php';
+require './Models/Gioco.php';
+
+
+//oggetti cane e gatto  della classe categoria.
+$cane = new Categoria("cane","2 anni","30kg");
+ //var_dump($cane);
+$gatto = new Categoria("gatto","1 anni","2kg");
+//var_dump($gatto);
+
+
+//oggetto prod1 della classe prodotto
+$prod1 = new Prodotto("yes","23.90", $cane);
+ //var_dump($prod1);
+
+
+
+//oggetti delle classi cibo e gioco che hanno ereditato alcune proprietà dalla classe prodotto.
+
+$cibo1 = new Cibo("croccantini","11.99",$cane);
+ //var_dump($cibo1);
+
+$gioco1 = new Gioco("osso finto","20",$gatto);
+//var_dump($gioco1);
+
+
+//creo un array che contiene tutti i miei prodotti
+$prodotti=[
+    $cibo1,
+    $gioco1,
+];
+
+var_dump($prodotti);
+
+
+?>
--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php
-
-require './Models/Prodotto.php';
-require './Models/Categoria.php';
-require './Models/Cibo.php';
-require './Models/Gioco.php';
-
-
-//oggetti cane e gatto  della classe categoria.
-$cane = new Categoria("cane","2 anni","30kg");
- //var_dump($cane);
-$gatto = new Categoria("gatto","1 anni","2kg");
-//var_dump($gatto);
-
-
-//oggetto prod1 della classe prodotto
-$prod1 = new Prodotto("yes","23.90", $cane);
-var_dump($prod1);
-
-
-
-//oggetti delle classi cibo e gioco che hanno ereditato alcune proprietà dalla classe prodotto.
-
-$cibo1 = new Cibo("croccantini","11.99",$cane);
-var_dump($cibo1);
-
-$gioco1 = new Gioco("osso finto","20",$gatto);
-var_dump($gioco1);
-
+require './db.php';
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -43,14 +11,9 @@ var_dump($gioco1);
 </head>
 <body>
     <h1>titolo2</h1>
-    <!--
-        1-i prodotti HANNO una categoria(composizione)
-          -immagine
-          -titolo
-          -prezzo
-          -icona
+    
 
-        2-i prodott cibo, giochi e cucce SONO prodotti (ereditarietà)
-    -->
+
+
 </body>
 </html>
