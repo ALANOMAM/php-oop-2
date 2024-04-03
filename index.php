dcaf980ecad23b12ce3dd406afef13beea720c6f
alan_omam <omamalan@hotmail.it>
2024-04-03 16:00:26 +0200

    esposto cane e gatto come oggetti della classe categoria

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
 require './Models/Prodotto.php';
+require './Models/Categoria.php';
 
+//oggetto prod1 della classe prodotto
 $prod1 = new Prodotto("yes","23.90");
 var_dump($prod1);
 
+//oggetti cane e gatto  della classe categoria.
+$cane = new Categoria("cane","2 anni","30kg");
+var_dump($cane);
+$gatto = new Categoria("gatto","1 anni","2kg");
+var_dump($gatto);
 ?>
 
 
