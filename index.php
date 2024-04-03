296949598d2f5b9ee2c25f7a2fc12203b64fdebe
alan_omam <omamalan@hotmail.it>
2024-04-03 15:22:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP2</title>
+</head>
+<body>
+    <h1>titolo2</h1>
+    <!--
+        1-i prodotti HANNO una categoria(composizione)
+          -immagine
+          -titolo
+          -prezzo
+          -icona
+
+        2-i prodott cibo, giochi e cucce SONO prodotti (ereditarietà)
+    -->
+</body>
+</html>
